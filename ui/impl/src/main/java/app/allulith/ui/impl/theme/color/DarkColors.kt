208ca26487e5169f4244004e73316219b86ca189
Talia Woodroof <taliawoodroof@gmail.com>
2025-11-04 18:29:10 +0000

    improve: colors

--- ui/impl/src/main/java/app/allulith/ui/impl/theme/color/DarkColors.kt
+++ ui/impl/src/main/java/app/allulith/ui/impl/theme/color/DarkColors.kt
@@ -3,6 +3,6 @@ package app.allulith.ui.impl.theme.color
 import androidx.compose.ui.graphics.Color
 
 internal val darkColors = OrganiserColorScheme(
-    primary = Color(0xFFFFFFFF),
-    background = Color.Red,
+    primary = Color(0xFFECECEC),
+    background = Color(0xFF000000),
 )
--- ui/impl/src/main/java/app/allulith/ui/impl/theme/color/LightColors.kt
+++ ui/impl/src/main/java/app/allulith/ui/impl/theme/color/LightColors.kt
@@ -3,6 +3,6 @@ package app.allulith.ui.impl.theme.color
 import androidx.compose.ui.graphics.Color
 
 internal val lightColors = OrganiserColorScheme(
-    primary = Color(0xFFFFFFFF),
-    background = Color.Red,
+    primary = Color(0xFF000000),
+    background = Color(0xFFECECEC),
 )
