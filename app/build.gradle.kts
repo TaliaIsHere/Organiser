f8e261dd5b0a26e3f1d98aea4f6440a9abde8cf5
Talia Woodroof <taliawoodroof@gmail.com>
2025-11-04 15:41:42 +0000

    tidy: gradle files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,19 +50,14 @@ android {
 dependencies {
     implementation(project(":ui:impl"))
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
+    implementation(libs.bundles.core.ui)
+    implementation(libs.bundles.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+
+    testImplementation(libs.bundles.test)
+
+    androidTestImplementation(libs.bundles.android.test)
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    debugImplementation(libs.bundles.debug)
 }
--- ui/impl/build.gradle.kts
+++ ui/impl/build.gradle.kts
@@ -12,13 +12,6 @@ android {
         version = release(36)
     }
 
-    defaultConfig {
-        minSdk = 34
-        targetSdk = 36
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -46,8 +39,7 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
+    implementation(libs.bundles.core.ui)
+    implementation(libs.bundles.compose)
     implementation(platform(libs.androidx.compose.bom))
 }
