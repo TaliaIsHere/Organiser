d0722f978c0b03b98e34eca7d08256aba657472a
Talia Woodroof <taliawoodroof@gmail.com>
2025-12-08 13:19:47 +0000

    feat: add splash screen
    Also adjusted dark background to remove some slight blueness

--- ui/impl/src/main/java/app/allulith/ui/impl/theme/color/DarkColors.kt
+++ ui/impl/src/main/java/app/allulith/ui/impl/theme/color/DarkColors.kt
@@ -4,7 +4,7 @@ import androidx.compose.ui.graphics.Color
 
 internal val darkColors = OrganiserColorScheme(
     primary = Color(0xFF79C99E),
-    onPrimary = Color(0xFF04030F),
-    background = Color(0xFF04030F),
+    onPrimary = Color(0xFF040308),
+    background = Color(0xFF040308),
     onBackground = Color(0xFFF2EFE9),
 )
--- ui/impl/src/main/java/app/allulith/ui/impl/theme/color/LightColors.kt
+++ ui/impl/src/main/java/app/allulith/ui/impl/theme/color/LightColors.kt
@@ -4,7 +4,7 @@ import androidx.compose.ui.graphics.Color
 
 internal val lightColors = OrganiserColorScheme(
     primary = Color(0xFF88F1B0),
-    onPrimary = Color(0xFF04030F),
+    onPrimary = Color(0xFF040308),
     background = Color(0xFFF2EFE9),
-    onBackground = Color(0xFF04030F),
+    onBackground = Color(0xFF040308),
 )
