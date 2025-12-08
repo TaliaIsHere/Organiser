1fbe6b734179afacbc68b8e9e86055f07f756b72
Talia Woodroof <taliawoodroof@gmail.com>
2025-12-08 15:22:55 +0000

    tidy: move string literals to strings.xml

--- signup/impl/src/main/java/app/allulith/signup/impl/ui/destinations/accountCreation/AccountCreationRoute.kt
+++ signup/impl/src/main/java/app/allulith/signup/impl/ui/destinations/accountCreation/AccountCreationRoute.kt
@@ -1,7 +1,9 @@
 package app.allulith.signup.impl.ui.destinations.accountCreation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.PreviewLightDark
+import app.allulith.signup.impl.R
 import app.allulith.ui.impl.templates.OrganiserScreen
 import app.allulith.ui.impl.templates.OrganiserScreenAction
 import app.allulith.ui.impl.theme.OrganiserTheme
@@ -18,10 +20,10 @@ private fun AccountCreationScreen(
     onContinue: () -> Unit,
 ) {
     OrganiserScreen(
-        header = "Time to create an account!",
-        description = "TODO",
+        header = stringResource(R.string.signup_account_creation_header),
+        description = stringResource(R.string.signup_account_creation_description),
         primaryAction = OrganiserScreenAction(
-            text = "Create",
+            text = stringResource(R.string.signup_account_creation_button_text),
             onClick = onContinue,
         )
     ) {
--- signup/impl/src/main/java/app/allulith/signup/impl/ui/destinations/welcome/WelcomeRoute.kt
+++ signup/impl/src/main/java/app/allulith/signup/impl/ui/destinations/welcome/WelcomeRoute.kt
@@ -1,7 +1,9 @@
 package app.allulith.signup.impl.ui.destinations.welcome
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.PreviewLightDark
+import app.allulith.signup.impl.R
 import app.allulith.ui.impl.templates.OrganiserScreen
 import app.allulith.ui.impl.templates.OrganiserScreenAction
 import app.allulith.ui.impl.theme.OrganiserTheme
@@ -18,10 +20,10 @@ private fun WelcomeScreen(
     onContinue: () -> Unit,
 ) {
     OrganiserScreen(
-        header = "Welcome to Organiser!",
-        description = "There are a few things to set-up before we can start",
+        header = stringResource(R.string.signup_welcome_header),
+        description = stringResource(R.string.signup_welcome_description),
         primaryAction = OrganiserScreenAction(
-            text = "Get started!",
+            text = stringResource(R.string.signup_welcome_button_text),
             onClick = onContinue,
         )
     ) {
